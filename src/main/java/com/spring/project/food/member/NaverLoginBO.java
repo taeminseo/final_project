bca9ca648bc59f2fca59fbbdb482e4d95572d7ec
Taemin Seo <88878395+taeminseo@users.noreply.github.com>
2022-07-12 10:30:10 +0900

    Update NaverLoginBO.java

--- src/main/java/com/spring/project/food/member/NaverLoginBO.java
+++ src/main/java/com/spring/project/food/member/NaverLoginBO.java
@@ -24,9 +24,9 @@ public class NaverLoginBO {
     //response_type: 인증 과정에 대한 구분값. code로 값이 고정돼 있습니다.
     //redirect_uri: 네이버 로그인 인증의 결과를 전달받을 콜백 URL(URL 인코딩). 애플리케이션을 등록할 때 Callback URL에 설정한 정보입니다.
     //state: 애플리케이션이 생성한 상태 토큰
-    private String CLIENT_ID = "r1SmpcOU9WXbw9BQzR05";
-    private String CLIENT_SECRET = "9YRZW9zA_T";
-    private String REDIRECT_URI = "http://localhost:8085";
+    private String CLIENT_ID = "client_id";
+    private String CLIENT_SECRET = "client_secret";
+    private String REDIRECT_URI = "redirect_uri";
     private String SESSION_STATE = "oauth_state";
     /* 프로필 조회 API URL */
     private String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
